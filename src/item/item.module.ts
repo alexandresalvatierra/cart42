8b431039e6750f421a37f02855dacfa3a37f786b
alexandresalvatierra <alexandre.salvatierra@gmail.com>
2023-11-06 02:41:18 -0300

    feat: add delete route controller

--- src/item/item.module.ts
+++ src/item/item.module.ts
@@ -7,5 +7,6 @@ router.post('/', itemController.create)
 router.get('/', itemController.index)
 router.get('/:id', itemController.show)
 router.put('/:id', itemController.update)
+router.delete('/:id', itemController.destroy)
 
 export default router
