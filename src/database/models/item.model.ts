7c24c6fbf4cc52ddc0e974b6502ae0ac51494e98
alexandresalvatierra <alexandre.salvatierra@gmail.com>
2023-11-06 02:41:00 -0300

    feat: remove id item model

--- src/database/models/item.model.ts
+++ src/database/models/item.model.ts
@@ -1,12 +1,11 @@
 import mongoose, { Schema } from 'mongoose'
-import crypto from 'crypto'
 
 const ItemSchema: Schema = new Schema({
-  id: { type: Schema.Types.UUID, unique: true, default: crypto.randomUUID() },
   name: { type: String, unique: true, required: false },
   price: { type: Number, required: true },
   quantity: { type: Number, required: true },
   created_at: { type: Date, required: true },
+  updated_at: { type: Date, required: true },
 })
 
 export const ItemModel = mongoose.model('Item', ItemSchema)
